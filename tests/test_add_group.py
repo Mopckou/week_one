6ef82eaca773ac8834e037bc5957c9140d280a7f
Mopckou <ermakov.a@list.ru>
2017-06-08 02:22:40 +0300

    Добавлена параметризация

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,24 +1,26 @@
 # -*- coding: utf-8 -*-
 
 from model.group import Group
+import pytest
+import random
+import string
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
+testdata = [Group(name="", header="", footer="")] +[
+    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
+    for i in range(5)
+]
 
-def test_group_add(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name="3", header="33", footer="333")
-    app.group.create(group)
-    assert len(old_groups) + 1 == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_map) == sorted(new_groups, key=Group.id_or_map)
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_group_add(app, group):
+     old_groups = app.group.get_group_list()
+     app.group.create(group)
+     assert len(old_groups) + 1 == app.group.count()
+     new_groups = app.group.get_group_list()
+     old_groups.append(group)
+     assert sorted(old_groups, key=Group.id_or_map) == sorted(new_groups, key=Group.id_or_map)
 
-# def test_group_empty_add(app):
-#     old_groups = app.group.get_group_list()
-#     group = Group(name="", header="", footer="")
-#     app.group.create(group)
-#     new_groups = app.group.get_group_list()
-#     assert len(old_groups) + 1 == len(new_groups)
-#     old_groups.append(group)
-#     assert sorted(old_groups, key=Group.id_or_map) == sorted(new_groups, key=Group.id_or_map)
 
 
